8ffdb5266ea44b1bf686fe7df5cb0b8cc1d7d840
Ammon Werner <84041746+SonOfMosiah@users.noreply.github.com>
2022-10-14 14:01:52 -0600

    Update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,64 +1,39 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "@nomicfoundation/hardhat-chai-matchers";
-import "@nomiclabs/hardhat-solhint";
-import "hardhat-docgen";
-import "xdeployer";
-import "hardhat-spdx-license-identifier";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import '@nomicfoundation/hardhat-chai-matchers';
+import '@nomiclabs/hardhat-solhint';
+import 'hardhat-docgen';
+import 'hardhat-spdx-license-identifier';
 
-import * as dotenv from "dotenv";
+import { networks } from './hardhat.networks';
+
+networks['hardhat'] = {
+    forking: {
+        url: process.env.MAINNET_RPC_URL || '',
+    },
+};
+
+import * as dotenv from 'dotenv';
 dotenv.config();
 
 const config: HardhatUserConfig = {
     solidity: {
-        version: "0.8.15",
+        version: '0.8.16',
         settings: {
             optimizer: {
                 enabled: true,
                 runs: 1000,
             },
             outputSelection: {
-                "*": {
-                    "*": ["storageLayout"],
+                '*': {
+                    '*': ['storageLayout'],
                 },
             },
         },
     },
-    networks: {
-        aurora: {
-            url: process.env.AURORA_URL || "",
-            accounts:
-                process.env.PRIVATE_KEY !== undefined
-                    ? [process.env.PRIVATE_KEY]
-                    : [],
-        },
-        testnet: {
-            url: process.env.TESTNET_URL || "",
-            accounts:
-                process.env.PRIVATE_KEY !== undefined
-                    ? [process.env.PRIVATE_KEY]
-                    : [],
-        },
-        hardhat: {
-            forking: {
-                url: process.env.AURORA_URL || "",
-            },
-        },
-    },
-    // xdeployer address: 0x13b0D85CcB8bf860b6b79AF3029fCA081AE9beF2
-    // production networks: mainnet, bsc, optimism, arbitrum, polygon, heco, fantom, avalanche, gnosis, moonriver, moonbeam, celo, aurora, harmony, autobahn, fuse, cronos, evmos, boba
-    // testnets: rinkeby, ropsten, kovan, goerli, sepolia, bscTestnet, arbitrumTetnet, mumbai, hecoTestnet, fantomTestnet, fuji, sokol, moonbaseAlpha, alfajores, auroraTestnet, spark, cronosTestnet, evmosTestnet, bobaTestnet
-    xdeploy: {
-        contract: process.env.CONTRACT_NAME,
-        constructorArgsPath: process.env.CONTRACT_CONSTRUCTOR_ARGS_PATH,
-        salt: process.env.SALT,
-        signer: process.env.PRIVATE_KEY,
-        networks: ["LIST_OF_NETWORKS"],
-        rpcUrls: ["LIST_OF_RPCURLS"],
-        // gasLimit: "GAS_LIMIT", // optional
-    },
+    networks,
     docgen: {
-        path: "./docs",
+        path: './docs',
         clear: true,
         runOnCompile: true,
     },
--- hardhat.networks.ts
+++ hardhat.networks.ts
@@ -1,13 +1,32 @@
-const networks = {};
-const etherscan = { apiKey: {} };
+export const networks: {
+    [name: string]: Network;
+} = {};
+const etherscan: Etherscan = {};
+
+interface Etherscan {
+    [apiKey: string]: ApiKey;
+}
+
+interface ApiKey {
+    [network: string]: string;
+}
+
+interface Network {
+    url?: string;
+    chainId?: number;
+    accounts?: string[];
+    forking?: {
+        url: string;
+    };
+}
 
 function register(
-    name,
-    chainId,
-    url,
-    privateKey,
-    etherscanNetworkName,
-    etherscanKey
+    name: string,
+    chainId: number,
+    url: string,
+    privateKey: string,
+    etherscanNetworkName: string,
+    etherscanKey: string
 ) {
     if (url && privateKey && etherscanKey) {
         networks[name] = {
@@ -23,84 +42,92 @@ function register(
 }
 
 register(
-    "mainnet",
+    'mainnet',
     1,
-    process.env.MAINNET_RPC_URL,
-    process.env.MAINNET_PRIVATE_KEY,
-    "mainnet",
-    process.env.MAINNET_ETHERSCAN_KEY
+    process.env.MAINNET_RPC_URL!,
+    process.env.MAINNET_PRIVATE_KEY!,
+    'mainnet',
+    process.env.MAINNET_ETHERSCAN_KEY!
 );
 register(
-    "bsc",
+    'bsc',
     56,
-    process.env.BSC_RPC_URL,
-    process.env.BSC_PRIVATE_KEY,
-    "bsc",
-    process.env.BSC_ETHERSCAN_KEY
+    process.env.BSC_RPC_URL!,
+    process.env.BSC_PRIVATE_KEY!,
+    'bsc',
+    process.env.BSC_ETHERSCAN_KEY!
 );
 register(
-    "kovan",
-    42,
-    process.env.KOVAN_RPC_URL,
-    process.env.KOVAN_PRIVATE_KEY,
-    "kovan",
-    process.env.KOVAN_ETHERSCAN_KEY
+    'goerli',
+    420,
+    process.env.GOERLI_RPC_URL!,
+    process.env.GOERLI_PRIVATE_KEY!,
+    'goerli',
+    process.env.GOERLI_ETHERSCAN_KEY!
 );
 register(
-    "optimistic",
+    'sepolia',
+    11155111,
+    process.env.SEPOLIA_RPC_URL!,
+    process.env.SEPOLIA_PRIVATE_KEY!,
+    'sepolia',
+    process.env.SEPOLIA_ETHERSCAN_KEY!
+);
+register(
+    'optimism',
     10,
-    process.env.OPTIMISTIC_RPC_URL,
-    process.env.OPTIMISTIC_PRIVATE_KEY,
-    "optimisticEthereum",
-    process.env.OPTIMISTIC_ETHERSCAN_KEY
+    process.env.OPTIMISM_RPC_URL!,
+    process.env.OPTIMISM_PRIVATE_KEY!,
+    'optimisticEthereum',
+    process.env.OPTIMISTIC_ETHERSCAN_KEY!
 );
 register(
-    "matic",
+    'polygon',
     137,
-    process.env.MATIC_RPC_URL,
-    process.env.MATIC_PRIVATE_KEY,
-    "polygon",
-    process.env.MATIC_ETHERSCAN_KEY
+    process.env.POLYGON_RPC_URL!,
+    process.env.POLYGON_PRIVATE_KEY!,
+    'polygon',
+    process.env.POLYGON_ETHERSCAN_KEY!
 );
 register(
-    "arbitrum",
+    'arbitrum',
     42161,
-    process.env.ARBITRUM_RPC_URL,
-    process.env.ARBITRUM_PRIVATE_KEY,
-    "arbitrumOne",
-    process.env.ARBITRUM_ETHERSCAN_KEY
+    process.env.ARBITRUM_RPC_URL!,
+    process.env.ARBITRUM_PRIVATE_KEY!,
+    'arbitrumOne',
+    process.env.ARBITRUM_ETHERSCAN_KEY!
 );
 register(
-    "xdai",
+    'xdai',
     100,
-    process.env.XDAI_RPC_URL,
-    process.env.XDAI_PRIVATE_KEY,
-    "xdai",
-    process.env.XDAI_ETHERSCAN_KEY
+    process.env.XDAI_RPC_URL!,
+    process.env.XDAI_PRIVATE_KEY!,
+    'xdai',
+    process.env.XDAI_ETHERSCAN_KEY!
 );
 register(
-    "avax",
+    'avax',
     43114,
-    process.env.AVAX_RPC_URL,
-    process.env.AVAX_PRIVATE_KEY,
-    "avalanche",
-    process.env.AVAX_ETHERSCAN_KEY
+    process.env.AVAX_RPC_URL!,
+    process.env.AVAX_PRIVATE_KEY!,
+    'avalanche',
+    process.env.AVAX_ETHERSCAN_KEY!
 );
 register(
-    "fantom",
+    'fantom',
     250,
-    process.env.FANTOM_RPC_URL,
-    process.env.FANTOM_PRIVATE_KEY,
-    "opera",
-    process.env.FANTOM_ETHERSCAN_KEY
+    process.env.FANTOM_RPC_URL!,
+    process.env.FANTOM_PRIVATE_KEY!,
+    'opera',
+    process.env.FANTOM_ETHERSCAN_KEY!
 );
 register(
-    "aurora",
+    'aurora',
     1313161554,
-    process.env.AURORA_RPC_URL,
-    process.env.AURORA_PRIVATE_KEY,
-    "aurora",
-    process.env.AURORA_ETHERSCAN_KEY
+    process.env.AURORA_RPC_URL!,
+    process.env.AURORA_PRIVATE_KEY!,
+    'aurora',
+    process.env.AURORA_ETHERSCAN_KEY!
 );
 // register('ropsten', 3, process.env.ROPSTEN_RPC_URL, process.env.ROPSTEN_PRIVATE_KEY);
 // register('kovan-optimistic', 69, process.env.KOVAN_OPTIMISTIC_RPC_URL, process.env.KOVAN_OPTIMISTIC_PRIVATE_KEY);
